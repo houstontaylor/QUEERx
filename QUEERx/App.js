e89dfee073988b1fc10f29d96e1671f99f030003
Taylor96217 <taylor96217@gmail.com>
2023-12-01 03:06:22 -0800

    test

--- QUEERx/App.js
+++ QUEERx/App.js
@@ -1,12 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { PaperProvider, Searchbar } from 'react-native-paper';
+import { MaterialIcons } from '@expo/vector-icons'; 
 
 export default function App() {
+  const [searchQuery, setSearchQuery] = React.useState('');
+
+  const onChangeSearch = query => setSearchQuery(query);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <PaperProvider>
+      <View style={styles.container}>
+        <Searchbar
+          placeholder="Search"
+          onChangeText={onChangeSearch}
+          value={searchQuery}
+        />
+        <StatusBar style="auto" />
+      </View>
+    </PaperProvider>
   );
 }
 
