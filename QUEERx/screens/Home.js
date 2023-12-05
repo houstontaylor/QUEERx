9fa9c81fc5294feb5d24d8a6cfc4babc4d8a59a8
Taylor96217 <taylor96217@gmail.com>
2023-12-05 02:09:55 -0800

    almost

--- QUEERx/screens/Home.js
+++ QUEERx/screens/Home.js
@@ -1,69 +1,105 @@
-import React from "react";
-import { View, Linking } from "react-native";
-import {
-  Layout,
-  TopNav,
-  Text,
+import React, { useState } from 'react';
+import { View, StyleSheet, Image, ScrollView } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { Provider as PaperProvider, 
+  Searchbar, 
+  List, 
+  Title,
   Button,
-  Section,
-  SectionContent,
-  useTheme,
-  themeColor,
-} from "react-native-paper";
-import { Ionicons } from "@expo/vector-icons";
-import AppNavigator from "./src/navigation/AppNavigator";
+  Card,
+  Text,
+  Modal,
+  Portal,
+  Checkbox,
+  IconButton } from 'react-native-paper';
+import StarRating from 'react-native-star-rating';
+import { MaterialCommunityIcons } from '@expo/vector-icons'; 
 
-export default function ({ navigation }) {
-  const { isDarkmode, setTheme } = useTheme();
+const Home = ({ navigation }) => {
+  const [searchQuery, setSearchQuery] = useState('');
+  const [location, setLocation] = useState('');
+
+  const handleSearch = () => {
+    // Navigate to the Search Results screen with searchQuery and location as parameters
+    navigation.navigate("Results", { searchQuery, location });
+  };
+
   return (
-    <Layout>
-      <TopNav
-        middleContent="Home"
-        rightContent={
-          <Ionicons
-            name={isDarkmode ? "sunny" : "moon"}
-            size={20}
-            color={isDarkmode ? themeColor.white100 : themeColor.dark}
-          />
-        }
-        rightAction={() => {
-          if (isDarkmode) {
-            setTheme("light");
-          } else {
-            setTheme("dark");
-          }
-        }}
+    <View style={styles.container}>
+      <Image source={require('../assets/logo.png')} style={styles.image} />
+      <Searchbar
+        placeholder="Search doctor by name or specialty"
+        onChangeText={(text) => setSearchQuery(text)}
+        value={searchQuery}
+        onSubmitEditing={handleSearch}
+        style={styles.searchBar}
       />
-      <View
-        style={{
-          flex: 1,
-          alignItems: "center",
-          justifyContent: "center",
-        }}
-      >
-        <Section>
-          <SectionContent>
-            <Text fontWeight="bold" style={{ textAlign: "center" }}>
-              These UI components provided by Rapi UI
-            </Text>
-            <Button
-              style={{ marginTop: 10 }}
-              text="Rapi UI Documentation"
-              status="info"
-              onPress={() => Linking.openURL("https://rapi-ui.kikiding.space/")}
-            />
-            <Button
-              text="Go to second screen"
-              onPress={() => {
-                navigation.navigate("SecondScreen");
-              }}
-              style={{
-                marginTop: 10,
-              }}
-            />
-          </SectionContent>
-        </Section>
-      </View>
-    </Layout>
+      <Searchbar
+        placeholder="Current location"
+        onChangeText={(text) => setLocation(text)}
+        value={location}
+        onSubmitEditing={handleSearch}
+        style={styles.searchBar}
+        icon="map-marker"
+      />
+      <Button mode="contained" onPress={handleSearch} style={styles.button}>
+        Search
+      </Button>
+      <Image source={require('../assets/doctors.png')} style={styles.image} />
+    </View>
   );
-}
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 16,
+  },
+  button: {
+    marginTop: 16,
+    backgroundColor: '#306B70',
+
+  },
+  image: {
+    width: '90%',
+  },
+  resultContainer: {
+    marginBottom: 16,
+  },
+  doctorimage: {
+    width: 70, // Adjust the size as needed
+    height: 70, // Adjust the size as needed
+    resizeMode: 'cover',
+    position: 'absolute',
+    top: 25,
+    left: 10,
+    borderRadius: 8,
+  },
+  searchBar: {
+    marginBottom: 16,
+    backgroundColor: 'rgba(148,206,210,0.30)',
+    width: '100%',
+  },
+  card: {
+    marginBottom: 16,
+
+    height: '14%'
+  },
+  textContainer: {
+    marginLeft: 80, // Adjust the margin to create space for the image
+  },
+  stars: {
+    marginRight: 8,
+  },
+  starsContainer: {
+    top: 25,
+    right: 5,
+    flexDirection: 'row-reverse', // Right-align the stars
+    alignItems: 'center',
+  }
+});
+
+export default Home;
