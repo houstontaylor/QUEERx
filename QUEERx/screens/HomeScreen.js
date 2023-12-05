58f8fd8a69830dd8665874c709c08d80c89ab7fe
Taylor96217 <taylor96217@gmail.com>
2023-12-05 11:27:41 -0800

    Home

--- QUEERx/screens/HomeScreen.js
+++ QUEERx/screens/HomeScreen.js
@@ -3,35 +3,89 @@ import React, { useState } from 'react';
 import { View, Text, TextInput, Button } from 'react-native';
 
 function HomeScreen({ navigation }) {
-  const [searchTerm, setSearchTerm] = useState('');
+  const [searchQuery, setSearchQuery] = useState('');
   const [location, setLocation] = useState('');
 
   const handleSearch = () => {
-    // Perform the search or navigation to the ResultsScreen with the search and location data
-    navigation.navigate('Results', { searchTerm, location });
+    // Navigate to the Search Results screen with searchQuery and location as parameters
+    navigation.navigate("Results", { searchQuery, location });
   };
 
   return (
-    <View style={{ padding: 16 }}>
-      <Text>Find a Doctor</Text>
-      
-      <TextInput
-        placeholder="Search for a doctor or specialty"
-        value={searchTerm}
-        onChangeText={(text) => setSearchTerm(text)}
-        style={{ marginTop: 10, marginBottom: 10, padding: 8, borderWidth: 1, borderColor: '#ccc' }}
+    <View style={styles.container}>
+      <Image source={require('../assets/logo.png')} style={styles.image} />
+      <Searchbar
+        placeholder="Search doctor by name or specialty"
+        onChangeText={(text) => setSearchQuery(text)}
+        value={searchQuery}
+        onSubmitEditing={handleSearch}
+        style={styles.searchBar}
       />
-
-      <TextInput
-        placeholder="Enter your location"
-        value={location}
+      <Searchbar
+        placeholder="Current location"
         onChangeText={(text) => setLocation(text)}
-        style={{ marginBottom: 10, padding: 8, borderWidth: 1, borderColor: '#ccc' }}
+        value={location}
+        onSubmitEditing={handleSearch}
+        style={styles.searchBar}
+        icon="map-marker"
       />
-
-      <Button title="Search" onPress={handleSearch} />
+      <Button mode="contained" onPress={handleSearch} style={styles.button}>
+        Search
+      </Button>
+      <Image source={require('../assets/doctors.png')} style={styles.image} />
     </View>
   );
 }
 
 export default HomeScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 16,
+  },
+  button: {
+    marginTop: 16,
+    backgroundColor: '#306B70',
+
+  },
+  image: {
+    width: '90%',
+  },
+  resultContainer: {
+    marginBottom: 16,
+  },
+  doctorimage: {
+    width: 70, // Adjust the size as needed
+    height: 70, // Adjust the size as needed
+    resizeMode: 'cover',
+    position: 'absolute',
+    top: 25,
+    left: 10,
+    borderRadius: 8,
+  },
+  searchBar: {
+    marginBottom: 16,
+    backgroundColor: 'rgba(148,206,210,0.30)',
+    width: '100%',
+  },
+  card: {
+    marginBottom: 16,
+
+    height: '14%'
+  },
+  textContainer: {
+    marginLeft: 80, // Adjust the margin to create space for the image
+  },
+  stars: {
+    marginRight: 8,
+  },
+  starsContainer: {
+    top: 25,
+    right: 5,
+    flexDirection: 'row-reverse', // Right-align the stars
+    alignItems: 'center',
+  }
+});
